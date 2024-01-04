6d1b962b648350681894fe888c9c63b5b6cc3a98
vicky262600 <vickyjoshi2604@gmail.com>
2024-01-03 23:03:41 -0500

    test 4

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -18,7 +18,7 @@ const baseURL = "https://rira-api.vercel.app";
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post(`${baseURL}/auth/login`, userCredential);
+    const res = await axios.post(`${baseURL}/api/auth/login`, userCredential);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
