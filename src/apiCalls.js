43760432375e071fb484b21712d1c446c293a218
vicky262600 <vickyjoshi2604@gmail.com>
2024-01-03 23:00:55 -0500

    edited apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,24 @@
+// import axios from "axios";
+
+// export const loginCall = async (userCredential, dispatch) => {
+//   dispatch({ type: "LOGIN_START" });
+//   try {
+//     const res = await axios.post("/auth/login", userCredential);
+//     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+//   } catch (err) {
+//     dispatch({ type: "LOGIN_FAILURE", payload: err });
+//   }
+// };
+
 import axios from "axios";
+
+// Assuming that your backend API is hosted at https://rira-api.vercel.app
+const baseURL = "https://rira-api.vercel.app";
 
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("/auth/login", userCredential);
+    const res = await axios.post(`${baseURL}/auth/login`, userCredential);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
